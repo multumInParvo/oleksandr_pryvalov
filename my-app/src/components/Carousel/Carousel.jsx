d6b29ac5545f793090810eab099207bd9f094d0a
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-05-16 00:54:30 +0200

    reworked and optimized code specially imgs

--- my-app/src/components/Carousel/Carousel.jsx
+++ my-app/src/components/Carousel/Carousel.jsx
@@ -1,21 +1,17 @@
 // Carousel.js
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import Chamigny from '../../images/chamigny_church.webp';
-import Duck from '../../images/duck.webp';
-import Helene from '../../images/helene.webp';
-import Chicken from '../../images/chicken.webp';
-import House from '../../images/house.webp';
-import Five from '../../images/five.webp';
+import Paintings from '../../data/Paintings';
 import '../Carousel/Carousel.scss';
 
-const images = [House, Duck, Helene, Five, Chicken, Chamigny];
 const intervalDuration = 6000;
+const images = Paintings.map((painting) => painting.picture);
 
 function Carousel() {
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
     const [fadeOut, setFadeOut] = useState(false);
     const [fadeIn, setFadeIn] = useState(true);
+
 
     useEffect(() => {
         const intervalId = setInterval(() => {
@@ -24,9 +20,9 @@ function Carousel() {
                 setCurrentImageIndex(prevIndex =>
                     prevIndex === images.length - 1 ? 0 : prevIndex + 1
                 );
-                setFadeIn(true); 
+                setFadeIn(true);
                 setFadeOut(false);
-            }, 1000); 
+            }, 1000);
         }, intervalDuration);
 
         return () => clearInterval(intervalId);
@@ -35,13 +31,13 @@ function Carousel() {
     return (
         <div className="carousel">
             <Link to="/works" className="image-container">
-                {images.map((image, index) => (
+                {Paintings.map((painting, index) => (
                     <img
                         key={index}
                         className={`carousel-image ${index === currentImageIndex ? (fadeIn ? 'fade-in' : '') : (fadeOut ? 'fade-out' : '')
                             }`}
-                        src={image}
-                        alt={`Image ${index + 1}`}
+                        src={painting.picture}
+                        alt={painting.description}
                     />
                 ))}
             </Link>
