e8f99813beddc828661785f2277692bc2190d268
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-05-16 01:06:40 +0200

    optimized imgs by importing component containing all images and its corresponding keys

--- my-app/src/pages/Works/Works.jsx
+++ my-app/src/pages/Works/Works.jsx
@@ -1,19 +1,13 @@
 // Works
 
 import React, { useState, useEffect } from 'react';
+import Paintings from '../../data/Paintings';
 import './Works.scss';
-import House from '../../images/house.webp';
-import Chamigny from '../../images/chamigny_church.webp';
-import Chicken from '../../images/chicken.webp';
-import Four from '../../images/four.webp';
-import Helene from '../../images/helene.webp';
-import Duck from '../../images/duck.webp';
 import Gallery from '../../components/Gallery/Gallery';
 
 function Works() {
     const [selectedImageIndex, setSelectedImageIndex] = useState(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const images = [Four, Chicken, Chamigny, House, Duck, Helene];
 
     const openModal = (index) => {
         setSelectedImageIndex(index);
@@ -27,11 +21,11 @@ function Works() {
     };
 
     const nextImage = () => {
-        setSelectedImageIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+        setSelectedImageIndex((prevIndex) => (prevIndex === Paintings.length - 1 ? 0 : prevIndex + 1));
     };
 
     const previousImage = () => {
-        setSelectedImageIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+        setSelectedImageIndex((prevIndex) => (prevIndex === 0 ? Paintings.length - 1 : prevIndex - 1));
     };
 
     useEffect(() => {
@@ -42,14 +36,14 @@ function Works() {
 
     return (
         <div className="gallery-container">
-            {images.map((image, index) => (
+            {Paintings.map((painting, index) => (
                 <div key={index} className="image-container" onClick={() => openModal(index)}>
-                    <img className='individual-painting' src={image} alt={`Image ${index + 1}`} />
+                    <img className='individual-painting' src={painting.picture} alt={painting.description} />
                 </div>
             ))}
             {isModalOpen && (
                 <Gallery
-                    selectedImage={images[selectedImageIndex]}
+                    painting={Paintings[selectedImageIndex]}
                     onClose={closeModal}
                     onNext={nextImage}
                     onPrevious={previousImage}
