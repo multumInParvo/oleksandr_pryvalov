7a82825836f2f5c9f823ccb6376c0526353a6a3f
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-05-16 01:01:54 +0200

    updated parts related to imgs

--- my-app/src/components/Gallery/Gallery.jsx
+++ my-app/src/components/Gallery/Gallery.jsx
@@ -1,19 +1,23 @@
-// Gallery.js
+// Gallery 
+
 import React from 'react';
 import '../Gallery/Gallery.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
-import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight, faXmark } from '@fortawesome/free-solid-svg-icons';
 
-function Gallery({ selectedImage, onClose, onNext, onPrevious }) {
-  
+function Gallery({ painting, onClose, onNext, onPrevious }) {
     return (
         <div className="gallery-modal">
             <span className="close-button" onClick={onClose}><FontAwesomeIcon icon={faXmark} /></span>
             <span className="button-left" onClick={onPrevious}><FontAwesomeIcon icon={faChevronLeft} /></span>
-            <img className="selected-image" src={selectedImage} alt="Selected Image" />
+            <img className="selected-image" src={painting.picture} alt={painting.description} />
             <span className="button-right" onClick={onNext}><FontAwesomeIcon icon={faChevronRight} /></span>
+            <div className="painting-details">
+                <p>{painting.title}<strong>|</strong></p>
+                <p>{painting.medium}<strong>|</strong></p>
+                <p>{painting.dimensions}<strong>|</strong></p>
+                <p>{painting.year}</p>
+            </div>
         </div>
     );
 }
