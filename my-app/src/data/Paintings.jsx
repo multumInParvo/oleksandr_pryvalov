6c427066284fa86f4d7449550631a7164d6247be
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-05-16 00:58:20 +0200

    created data folder with component inside containing keys and data related to images

---
+++ my-app/src/data/Paintings.jsx
@@ -0,0 +1,66 @@
+// Paintings
+
+import Chamigny from '../images/chamigny_church.webp';
+import Duck from '../images/duck.webp';
+import Helene from '../images/helene.webp';
+import Chicken from '../images/chicken.webp';
+import House from '../images/house.webp';
+import Leaves from '../images/leaves.webp';
+
+const PaintingsData = [
+    {
+        picture: Chamigny,
+        title: 'The red door',
+        medium: 'oil on canvas',
+        dimensions: '50x70',
+        year: '2024',
+        description: 'red door and beige wall in Chamigny',
+    },
+
+    {
+        picture: Duck,
+        title: 'A duck for Helene',
+        medium: 'oil on canvas',
+        dimensions: '30x40',
+        year: '2022',
+        description: 'A duck in red suit on green background',
+    },
+
+    {
+        picture: Helene,
+        title: 'Portrait of Helene',
+        medium: 'oil on canvas',
+        dimensions: '60x80',
+        year: '2024',
+        description: 'a woman in blue sitting reading on yellow armmchair',
+    },
+
+    {
+        picture: Chicken,
+        title: 'Chicken, owl and flowers',
+        medium: 'oil on canvas',
+        dimensions: '60x80',
+        year: '2024',
+        description: 'painting of a red porcelain chicken staring at a scared blue owl',
+    },
+
+    {
+        picture: House,
+        title: 'the house in Azay le rideau',
+        medium: 'oil on canvas',
+        dimensions: '60x80',
+        year: '2024',
+        description: 'old french houses with red doors',
+    },
+
+    {
+        picture: Leaves,
+        title: 'Red leaves',
+        medium: 'oil on canvas',
+        dimensions: '60x70',
+        year: '2024',
+        description: 'A tree with red leaves',
+    }
+];
+
+export default PaintingsData;
