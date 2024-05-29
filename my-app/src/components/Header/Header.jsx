a612f18d039243dc43555477b91ab8f5a458e38c
Oleksandr Pryvalov <oleksandrpryv@gmail.com>
2024-05-30 00:30:32 +0200

    adapted for mobile

--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,11 +1,35 @@
-// Header
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import '../Header/Header.scss';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const slideMenuRef = useRef(null);
+    const overlayRef = useRef(null);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const handleClickOutside = (event) => {
+        if (
+            (overlayRef.current && !overlayRef.current.contains(event.target)) &&
+            (!slideMenuRef.current || !slideMenuRef.current.contains(event.target))
+        ) {
+            setMenuOpen(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
+    }, []);
+
     return (
         <nav>
             <div className='logo'>
@@ -18,8 +42,25 @@ function Header() {
                 <Link to="/works" className='menu-links'>WORKS</Link>
                 <Link to="/about" className='menu-links'>ABOUT</Link>
                 <Link to="/contact" className='menu-links'>CONTACT</Link>
-                <Link to="https://www.instagram.com/oleksandrpryv/" className='instagram-logo'><FontAwesomeIcon icon={faInstagram} /></Link>
+                <Link to="https://www.instagram.com/oleksandrpryv/" className='instagram-logo'>
+                    <FontAwesomeIcon icon={faInstagram} />
+                </Link>
             </div>
+            <span className='menu-icon' onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faBars} />
+            </span>
+            <div className={`slide-menu ${menuOpen ? 'open' : ''}`} ref={slideMenuRef}>
+                <Link to="/works" className='menu-links' onClick={toggleMenu}>WORKS</Link>
+                <Link to="/about" className='menu-links' onClick={toggleMenu}>ABOUT</Link>
+                <Link to="/contact" className='menu-links' onClick={toggleMenu}>CONTACT</Link>
+                <Link to="https://www.instagram.com/oleksandrpryv/" className='instagram-logo' onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faInstagram} />
+                </Link>
+                <span className='close-icon' onClick={toggleMenu}>
+                    <FontAwesomeIcon icon={faXmark} />
+                </span>
+            </div>
+            <div className={`overlay ${menuOpen ? 'show' : ''}`} onClick={toggleMenu} ref={overlayRef}></div>
         </nav>
     );
 }
